27a2e6aeb6bb95c2b8dbfad8490e8974b8a1af95
lanpinggai666 <hy12121@outlook.com>
2025-11-14 13:15:31 +0800

    first commit

---
+++ WpfApp2/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        System.Threading.Mutex mutex;
+
+        public App()
+        {
+            this.Startup += new StartupEventHandler(App_Startup);
+        }
+
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            bool ret;
+            mutex = new System.Threading.Mutex(true, "WpfApp2", out ret);
+
+            if (!ret)
+            {
+                
+               Environment.Exit(0);
+            }
+
+        }
+    }
+}
+    
+
+
